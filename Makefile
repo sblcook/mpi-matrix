0bfb3050c965cb6bff554865b071e189a0797d2f
Paul Wolfgang <wolfgang@temple.edu>
2013-01-11 11:17:31 -0500

    Split mmult.c into three files

--- Makefile
+++ Makefile
@@ -1,9 +1,18 @@
-PGMS=mmult matrix_times_vector hello pi mxv_omp_mpi
+PGMS=mmult_omp_timing matrix_times_vector hello pi mxv_omp_mpi
 
 all:	${PGMS}
 
-mmult:	mmult.c
-	gcc -o mmult -fopenmp -O3 mmult.c
+mmult_omp_timing:	mmult.o mmult_omp.o mmult_omp_timing.o
+	gcc -o mmult -fopenmp -O3 mmult.o mmult_omp.o mmult_omp_timing.o -o mmult_omp_timing
+
+mmult.o:	mmult.c
+	gcc -c -O3 mmult.c
+
+mmult_omp.o:	mmult_omp.c
+	gcc -c -O3 -fopenmp mmult_omp.c
+
+mmult_omp_timing.o:	mmult_omp_timing.c
+	gcc -c -O3 mmult_omp_timing.c
 
 matrix_times_vector:	matrix_times_vector.c
 	mpicc -O3 -o matrix_times_vector matrix_times_vector.c
@@ -18,5 +27,6 @@ mxv_omp_mpi:	mxv_omp_mpi.c
 	mpicc -fopenmp -O3 -o mxv_omp_mpi mxv_omp_mpi.c
 
 clean:
+	rm -f *.o
 	rm -f ${PGMS}
 
---
+++ mmult_omp_timing.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/times.h>
+#include <stdlib.h>
+
+int mmult(double *c, 
+	  double *a, int aRows, int aCols, 
+	  double *b, int bRows, int bCols);
+
+int mmult_omp(double *c,
+		   double *a, int aRows, int aCols,
+		   double *b, int bRows, int bCols);
+
+double* gen_matrix(int n, int m) {
+  int i, j;
+  double *a = malloc(sizeof(double) * n * m);
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < m; j++) {
+      a[i*m + j] = rand()/RAND_MAX;
+    }
+  }
+  return a;
+}
+
+double deltaTime(struct timespec* start, struct timespec* end) {
+  double delta = (end->tv_sec - start->tv_sec) + (end->tv_nsec - start->tv_nsec)/1e9;
+  return delta;
+}
+
+int main(int argc, char* argv[]) {
+  struct timespec start;
+  struct timespec end;
+  struct timespec res;
+  double *a, *b, *c;
+  int n;
+  double times[2];
+  if (argc > 1) {
+    n = atoi(argv[1]);
+    a = gen_matrix(n, n);
+    b = gen_matrix(n, n);
+    c = malloc(sizeof(double) * n * n);
+    clock_gettime(CLOCK_REALTIME, &start);
+    mmult(c, a, n, n, b, n, n);
+    clock_gettime(CLOCK_REALTIME, &end);
+    times[0] = deltaTime(&start, &end);
+    printf("%d %f", n, times[0]);
+    clock_gettime(CLOCK_REALTIME, &start);
+    mmult_omp(c, a, n, n, b, n, n);
+    clock_gettime(CLOCK_REALTIME, &end);
+    times[1] = deltaTime(&start, &end);
+    printf(" %f", times[1]);
+    printf("\n");
+  } else {
+    fprintf(stderr, "Usage mmult n\n");
+  }
+}
+
