0371c6a5f6faead0b90b56ebc5316b7e73e280bc
Paul Wolfgang <wolfgang@temple.edu>
2013-01-19 14:37:52 -0500

    Added compare_matrices function to mmult.c and moved gen_matrix to mmult.c

--- mmult.c
+++ mmult.c
@@ -1,3 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
 int mmult(double *c, 
 	  double *a, int aRows, int aCols, 
 	  double *b, int bRows, int bCols) {
@@ -13,5 +17,31 @@ int mmult(double *c,
     }
   }
   return 0;
+}
+
+double* gen_matrix(int n, int m) {
+  int i, j;
+  double *a = malloc(sizeof(double) * n * m);
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < m; j++) {
+      a[i*m + j] = rand()/RAND_MAX;
+    }
+  }
+  return a;
+}
+
+void compare_matrices(double* a, double* b, int nRows, int nCols) {
+	int n = nRows * nCols;
+	int i, j, k;
+	for (k = 0; k < n; ++k) {
+		if (fabs(a[k]-b[k])/fabs(a[k]) > 1e-12) {
+			i = k/nCols;
+			j = k%nCols;
+			printf("a[%d][%d] == %.12g\nb[%d][%d] == %.12g\ndelta == %.12g\nrelerr == %.12g",
+				i, j, a[k], i, j, b[k], fabs(a[k]-b[k]), fabs(a[k]-b[k])/fabs(a[k]));
+			return;
+		}
+	}
+	printf("Matrices are the same\n");
 }
 
--- mmult_omp_timing.c
+++ mmult_omp_timing.c
@@ -11,16 +11,9 @@ int mmult_omp(double *c,
 		   double *a, int aRows, int aCols,
 		   double *b, int bRows, int bCols);
 
-double* gen_matrix(int n, int m) {
-  int i, j;
-  double *a = malloc(sizeof(double) * n * m);
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < m; j++) {
-      a[i*m + j] = rand()/RAND_MAX;
-    }
-  }
-  return a;
-}
+double* gen_matrix(int n, int m);
+
+void compare_matrices(double* a, double* b, int nRows, int nCols);
 
 double deltaTime(struct timespec* start, struct timespec* end) {
   double delta = (end->tv_sec - start->tv_sec) + (end->tv_nsec - start->tv_nsec)/1e9;
@@ -31,25 +24,27 @@ int main(int argc, char* argv[]) {
   struct timespec start;
   struct timespec end;
   struct timespec res;
-  double *a, *b, *c;
+  double *a, *b, *c1, *c2;
   int n;
   double times[2];
   if (argc > 1) {
     n = atoi(argv[1]);
     a = gen_matrix(n, n);
     b = gen_matrix(n, n);
-    c = malloc(sizeof(double) * n * n);
+    c1 = malloc(sizeof(double) * n * n);
+    c2 = malloc(sizeof(double) * n * n);
     clock_gettime(CLOCK_REALTIME, &start);
-    mmult(c, a, n, n, b, n, n);
+    mmult(c1, a, n, n, b, n, n);
     clock_gettime(CLOCK_REALTIME, &end);
     times[0] = deltaTime(&start, &end);
     printf("%d %f", n, times[0]);
     clock_gettime(CLOCK_REALTIME, &start);
-    mmult_omp(c, a, n, n, b, n, n);
+    mmult_omp(c2, a, n, n, b, n, n);
     clock_gettime(CLOCK_REALTIME, &end);
     times[1] = deltaTime(&start, &end);
     printf(" %f", times[1]);
     printf("\n");
+    compare_matrices(c1, c2, n, n);
   } else {
     fprintf(stderr, "Usage %s <n>\n", argv[0]);
   }
